82cc16d24fc48f7c4547fadeb6bf55b04e7d9ccf
athiVINAM <athira@vinamsolutions.com>
2025-04-04 21:46:10 +0530

    assignment1

---
+++ Assignment1Loopcase.java
@@ -0,0 +1,106 @@
+
+public class Assignment1Loopcase {
+
+	public static void main(String[] args) {
+		
+		//Assignment - all calendar months in the year
+		//switch cases and if else
+		
+		//Switch
+		byte month = 5;
+		switch(month)
+		{
+		case 1: 
+			System.out.println("jan");
+			break;
+		case 2: 
+			System.out.println("feb");
+			break;	
+		case 3:
+		System.out.println ("march");
+		break;
+		
+		case 4:
+			System.out.println("april");
+			break;
+		case 5: 
+			System.out.println("may");
+			break;
+		case 6: 
+			System.out.println("june");
+			break;	
+		case 7:
+		System.out.println ("july");
+		break;
+		
+		case 8:
+			System.out.println("august");
+			break;
+		case 9: 
+			System.out.println("sept");
+			break;
+		case 10: 
+			System.out.println("oct");
+			break;	
+		case 11:
+		System.out.println ("nov");
+		break;
+		
+		case 12:
+			System.out.println("dec");
+			break;
+				
+		
+		}
+		
+		//if else 
+		
+		byte month1 = 7;
+		if(month1 ==1)
+		{
+			System.out.println("jan");
+		}else if(month1 ==2)
+		{
+			System.out.println("feb");
+		}else if(month1 ==3)
+		{
+			System.out.println("march");
+		}else if(month1 ==4)
+		{
+			System.out.println("april");
+		}
+		else if (month1 ==5)
+		{
+			System.out.println("may");
+		}
+		else if (month1 == 6)
+		{
+			System.out.println("june");	
+		}
+		else if (month1 == 7)
+		{
+			System.out.println("july");	
+		}
+		else if (month1 == 8)
+		{
+			System.out.println("august");	
+		}
+		else if (month1 == 9)
+		{
+			System.out.println("sept");	
+		}
+		else if (month1 == 10)
+		{
+			System.out.println("oct");	
+		}
+		else if (month1 == 11)
+		{
+			System.out.println("nov");	
+		}
+		else if (month1 == 12)
+		{
+			System.out.println("dec");	
+		}
+	}
+
+}
